714b3ab2b0d55e1f9576153dd08c603aa8ed34c5
Daniel Razafsky <daniel@upte.ch>
2021-10-24 12:52:41 -0500

    feat: added ability to filter Wikipedia events by language, event type, page title, and user

--- src/app/pages/wikipedia/events/wikipedia-events/wikipedia-events.component.ts
+++ src/app/pages/wikipedia/events/wikipedia-events/wikipedia-events.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { fromEvent, Observable, Subscription } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, tap } from 'rxjs/operators';
 import { WikipediaEdit } from 'src/app/models/edit.model';
 import { WikipediaState } from 'src/app/reducers/wikipedia/wikipedia.reducers';
 import { selectEditsState } from 'src/app/selectors/wikipedia/wikipedia.selectors';
@@ -12,11 +13,15 @@ import { selectEditsState } from 'src/app/selectors/wikipedia/wikipedia.selector
   templateUrl: './wikipedia-events.component.html',
   styleUrls: ['./wikipedia-events.component.scss']
 })
-export class WikipediaEventsComponent implements OnInit {
+export class WikipediaEventsComponent implements OnInit, OnDestroy, AfterViewInit {
   @ViewChild('eventPaginator', { static: true })
-  private readonly pagniator!: MatPaginator;
+  private readonly pagniator!: MatPaginator
   
-  events$: Observable<ReadonlyArray<WikipediaEdit>> = this.store.select(selectEditsState)
+  @ViewChild('eventFilter', { static: true })
+  private readonly eventFilter!: ElementRef
+  
+  events$: Observable<ReadonlyArray<WikipediaEdit>> = this.store.select(selectEditsState({ filter: '' }))
+  subscriptions: Subscription = new Subscription();
 
   noData: Array<WikipediaEdit> = [<WikipediaEdit>{}]
   dataSource: MatTableDataSource<WikipediaEdit> = new MatTableDataSource<WikipediaEdit>(this.noData)
@@ -28,10 +33,34 @@ export class WikipediaEventsComponent implements OnInit {
   constructor(private readonly store: Store<WikipediaState>) { }
 
   ngOnInit(): void {
+    this.setup()
+  } 
+  
+  ngAfterViewInit(): void {
+    this.subscriptions = fromEvent(this.eventFilter.nativeElement, 'keyup')
+      .pipe(
+          filter(Boolean),
+          debounceTime(250),
+          distinctUntilChanged(),
+          tap(() => {
+            this.events$ = this.store.select(selectEditsState({ filter: this.eventFilter.nativeElement.value }))
+            this.setup()
+          })
+      )
+      .subscribe()
+  }
+  
+  ngOnDestroy(): void {
+    if (this.subscriptions) {
+      this.subscriptions.unsubscribe()
+    }
+  }
+  
+  private setup(): void {
     this.events$.subscribe(events => {
       const tableEvents = [...events]
       this.dataSource = new MatTableDataSource<WikipediaEdit>(tableEvents)
       this.dataSource.paginator = this.pagniator
     })
-  } 
+  }
 }
--- src/app/selectors/wikipedia/wikipedia.selectors.ts
+++ src/app/selectors/wikipedia/wikipedia.selectors.ts
@@ -4,11 +4,24 @@ import { WikipediaState } from "src/app/reducers/wikipedia/wikipedia.reducers";
 // Get the wikipedia state from the store
 export const selectWikipediaState = createFeatureSelector<WikipediaState>('wikipedia');
 
-// Get last 10 edits received
-export const selectEditsState = createSelector(
-  selectWikipediaState,
-  state => state.items
-)
+// Get all the edits received
+export const selectEditsState = (props: { filter: string }) => {
+  return createSelector(
+    selectWikipediaState,
+    state => {
+      if (!props.filter || props.filter === '') {
+        return state.items
+      }
+      
+      return state.items.filter(item => {
+        return item.country.includes(props.filter)
+        || item.event.includes(props.filter)
+        || item.item.includes(props.filter)
+        || item.user.includes(props.filter)
+      })
+    }
+  )
+}
 
 // Get total edit count
 export const selectEditCount = createSelector(
