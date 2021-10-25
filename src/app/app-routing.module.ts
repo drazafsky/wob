a445af715b6f8ec97016b596dc2204d7bae4e5b2
Daniel Razafsky <daniel@upte.ch>
2021-10-25 13:03:01 -0500

    fix: redirect / to /wikipedia

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'wikipedia', loadChildren: () => import('./pages/wikipedia/events/wikipedia-events/wikipedia-events.module').then(m => m.WikipediaEventsModule) }];
+const routes: Routes = [
+  {
+    path: 'wikipedia',
+    loadChildren: () => import('./pages/wikipedia/events/wikipedia-events/wikipedia-events.module').then(m => m.WikipediaEventsModule)
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'wikipedia'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
